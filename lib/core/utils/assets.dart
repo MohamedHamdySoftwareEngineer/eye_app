dfe94590d45b4f459169a495f8ee240e34d912ff
TheBestSoftwareEngineer <MOHAMEDHAMDY192001@gmail.com>
2025-02-17 12:13:45 +0200

    make starting box

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -6,4 +6,5 @@ class AssetsData {
   static const homeIcon = 'assets/images/home_icon.svg';
   static const trophyIcon = 'assets/images/trophy_icon.svg';
   static const userIcon = 'assets/images/user_icon.svg';
+  static const arrowIcon = 'assets/images/arrow_next.svg';
 }
--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -10,5 +10,6 @@ abstract class Styles {
 
   static const customtextStyle = TextStyle(
     fontSize: 16,
+    fontWeight: FontWeight.bold
   );
 }
