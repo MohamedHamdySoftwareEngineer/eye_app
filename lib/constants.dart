5b25caeda4414d70eb024c0209c7ba55ad24dcde
TheBestSoftwareEngineer <MOHAMEDHAMDY192001@gmail.com>
2025-02-18 12:21:34 +0200

    finish Strongest classifers text

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
 const mainColor = Color(0xffD3CCB8);
+const shadowMainColor = Color(0xffe9d7a2);
 const secondColor = Color(0xffD98E04);
+const shadowSecondColor = Color(0xfffcc96a);
 const textColor = Color(0xff594302);
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,11 +1,9 @@
-import 'package:eye/constants.dart';
 import 'package:eye/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:eye/features/home/presentation/views/widgets/starting_box.dart';
 import 'package:eye/features/home/presentation/views/widgets/welcome_text.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
-import '../../../../../core/utils/assets.dart';
+import '../../../../../constants.dart';
 import '../../../../../core/utils/styles.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -17,7 +15,39 @@ class HomeViewBody extends StatelessWidget {
       children: [
         CustomAppBar(),
         WelcomeText(),
+        SizedBox(
+          height: 40,
+        ),
         StartingBox(),
+        SizedBox(
+          height: 50,
+        ),
+        StrongestClassifiers(),
+      ],
+    );
+  }
+}
+
+class StrongestClassifiers extends StatelessWidget {
+  const StrongestClassifiers({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          'الأقوى',
+          style: Styles.customtextStyle.copyWith(
+            fontWeight: FontWeight.w900,
+            fontSize: 25,
+          shadows: [
+            const Shadow(
+              blurRadius: 7,
+              color: shadowMainColor,
+              offset: Offset(-1, 5),
+            )
+          ]),
+        ),
       ],
     );
   }
